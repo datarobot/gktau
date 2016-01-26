9ddcfbce09cff1de8e1a6af291666c783caf839e
Rob Pearson <ron@Rons-UX301LAA.(none)>
2016-01-26 11:17:34 -0500

    Checkpointing progress in GoodmanKruskal package

---
+++ R/GroupNumericDataframe.R
@@ -0,0 +1,21 @@
+#' Group numeric variables in a dataframe
+#'
+#' TODO: complete function code and documentation
+#'
+#' @inheritParams MakeDataframe
+#' @param groupList List of parameters that specify which
+#' numeric variables are to be grouped and the parameters
+#' used for grouping them; see Details.
+#' @return Dataframe with numeric variables specified by
+#' groupList grouped and converted to factors.
+#' @author Ron Pearson
+#' @export
+#'
+GroupNumericDataframe <- function(df, groupList){
+  #
+  #  TODO: implement this function - stub follows
+  #
+  message("GroupNumericDataframe function not yet implemented:
+           returning ungrouped dataframe.")
+  return(df)
+}
